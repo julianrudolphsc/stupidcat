cc77134b02ac98e4da270ff2b70ea3c271ca74ee
julian rudolph <julian.rudolphsc@gmail.com>
2016-08-07 09:23:09 -0600

    created header/footer partials, added bootstrap

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+//routes
+//home route
+app.get('/', function(req, res){
+  res.redirect('/cats');
+});
+
+//RESTful Routes
+//index /cats
+app.get('/cats', function(req, res){
+  res.render('cats');
+});
+
 //listener
 app.listen(3000, function(){
   console.log("Server Started!");
